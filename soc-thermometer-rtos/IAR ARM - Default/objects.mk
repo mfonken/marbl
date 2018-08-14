5b7c4104e73401d8051a0bcc4e1b42863d09b27a
mfonken <mgfonken@gmail.com>
2018-08-13 21:36:27 -0600

    Added rtos example project

---
+++ soc-thermometer-rtos/IAR ARM - Default/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := C:/Users/Matthew\ Fonken/SimplicityStudio/v4_workspace/soc-thermometer-rtos/protocol/bluetooth/lib/EFR32MG1P/IAR/libmbedtls.a C:/Users/Matthew\ Fonken/SimplicityStudio/v4_workspace/soc-thermometer-rtos/protocol/bluetooth/lib/EFR32MG1P/IAR/libpsstore.a C:/Users/Matthew\ Fonken/SimplicityStudio/v4_workspace/soc-thermometer-rtos/protocol/bluetooth/lib/EFR32MG1P/IAR/binapploader.o C:/Users/Matthew\ Fonken/SimplicityStudio/v4_workspace/soc-thermometer-rtos/protocol/bluetooth/lib/EFR32MG1P/IAR/libbluetooth.a C:/Users/Matthew\ Fonken/SimplicityStudio/v4_workspace/soc-thermometer-rtos/platform/radio/rail_lib/autogen/librail_release/librail_efr32xg1_iar_release.a
+
---
+++ soc-thermometer-rtos/IAR ARM - Default/platform/micrium_os/kernel/source/os_core.d
@@ -0,0 +1,95 @@
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\kernel\source\os_core.c
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\rtos_description.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\rtos_opt_def.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\kernel\include\os.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\cpu\include\cpu.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\cpu\include\cpu_def.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\rtos_path.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\em_device.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p732f256gm32.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\CMSIS\Include\core_cm4.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\stdint.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\ycheck.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\yvals.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\DLib_Defaults.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\DLib_Config_Normal.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\DLib_Product.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\CMSIS\Include\core_cmInstr.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\cmsis_iar.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\intrinsics.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\CMSIS\Include\core_cmFunc.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\CMSIS\Include\core_cmSimd.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\system_efr32mg1p.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_msc.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_emu.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_rmu.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_cmu.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_crypto.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_gpio_p.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_gpio.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_prs_ch.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_prs.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_ldma_ch.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_ldma.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_fpueh.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_gpcrc.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_timer_cc.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_timer.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_usart.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_leuart.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_letimer.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_cryotimer.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_pcnt.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_i2c.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_adc.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_acmp.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_idac.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_rtcc_cc.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_rtcc_ret.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_rtcc.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_wdog_pch.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_wdog.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_dma_descriptor.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_devinfo.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_romtable.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_prs_signals.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_dmareq.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_af_ports.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\Device\SiliconLabs\EFR32MG1P\Include\efr32mg1p_af_pins.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\cpu_cfg.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\cpu\include\cpu_port_sel.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\ports\source\iar\armv7m_cpu_port.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\rtos_err.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\lib_def.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\toolchains.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\rtos_err_cfg.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\cpu\include\cpu_int.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\ports\source\generic\armv6m_v7m_cpu_int_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\emlib\inc\em_core.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\emlib\inc\em_common.h
+platform/micrium_os/kernel/source/os_core.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\stdbool.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\lib_mem.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\common_cfg.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\lib_utils.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\rtos_version.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\os_cfg.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\rtos_cfg.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\kernel\include\os_type.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\kernel\include\os_trace.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\kernel\include\os_port_sel.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\ports\source\iar\armv7m_os_cpu.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\kernel\source\os_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\source\rtos\rtos_utils_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\rtos_utils.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\source\logging\logging_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\lib_str.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\lib_ascii.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\source\lib\lib_str_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\source\preprocessor\preprocessor_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\source\ring_buf\ring_buf_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\logging.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\common.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\source\logging\logging_chk_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\source\kal\kal_priv.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\kal.h
+platform/micrium_os/kernel/source/os_core.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\micrium_os\common\include\rtos_prio.h
