49f2fd576840c11de3f1b4f965f1db83e7fc7309
mfonken <mgfonken@gmail.com>
2019-03-03 00:14:28 -0700

    ...

--- soc-thermometer-rtos/IAR ARM - Default/platform/emlib/src/em_assert.d
+++
@@ -1,3 +0,0 @@
-platform/emlib/src/em_assert.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\emlib\src\em_assert.c
-platform/emlib/src/em_assert.o: C:\Users\Matthew\ Fonken\SimplicityStudio\v4_workspace\soc-thermometer-rtos\platform\emlib\inc\em_assert.h
-platform/emlib/src/em_assert.o: c:\Program\ Files\ (x86)\IAR\ Systems\Embedded\ Workbench\ 8.0\arm\inc\c\stdbool.h
